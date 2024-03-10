beede508d54b632828acb13ac8497e7a320cdcd9
Nikitos1865 <na2789@columbia.edu>
2024-03-10 12:48:03 -0400

    Added methods to accept guesses from the player and print the board based on the amount of guesses left

--- game.rb
+++ game.rb
@@ -6,7 +6,7 @@ require './board'
 class Game 
 include Constants
 
-    attr_accessor :computer, :board
+    attr_accessor :computer, :board, :player 
 
     def initialize(name)
         @board = Board.new
@@ -17,9 +17,23 @@ include Constants
 
     def display_board(num)
         for num in 0..num do
-            puts "#{board.get_row(num)} Exact Match: #{computer.exact_match(board.get_row(num))}, Color Match: #{computer.color_match(board.get_row(num))}"
+            arr_to_check = board.get_row(num)
+            puts "#{arr_to_check} Exact Match: #{computer.exact_match(arr_to_check)}, Color Match: #{computer.color_match(arr_to_check)-computer.exact_match(arr_to_check)}"
         end 
     end 
+
+    def show_colors
+        puts "The colors you may choose for your guess are as follows: "
+        puts "red, blue, yellow, green, purple, orange"
+    end
+    
+    def accept_guess(guess)
+        guess = guess.downcase.chomp.split(" ")
+        board.set_row(12-player.guesses_left, guess)
+        player.guesses_left -= 1
+    end 
+
+
 end 
 
 
@@ -29,8 +43,17 @@ game = Game.new("Bibo")
 
 p game.computer.computer_choice
 
-game.board.set_row(1, ["blue", "green", "yellow", "purple"])
 
-game.display_board(3)
+game.show_colors
+
+p game.player.guesses_left
+
+p game.accept_guess(gets)
+game.display_board(11-game.player.guesses_left)
+
+p game.accept_guess(gets)
+game.display_board(11-game.player.guesses_left)
+
+
 
 #Exact Matches: #{@computer.exact_match(@board.get_row(num))}, Color Matches: #{@board.color_match(@board.get_row(num))}"
--- player.rb
+++ player.rb
@@ -1,11 +1,12 @@
 
 
 class Player 
-    attr_accessor :name, :guesses_left
+    attr_accessor :name, :guesses_left, :points, :latest_guess
 
     def initialize(name)
         @name = name
         @guesses_left = 12
+        @points = 0
     end
 
 end
